d5f3276c6b02faed2fa2365a6c912b6c968a1cd8
aa <aa>
2025-08-04 01:11:52 +0900

    fix1

--- _app.py
+++ _app.py
@@ -1,16 +1,1 @@
 __version__ = "aaa"
-
-
-r"""
-master : A.               F
-          \             /
-fix    :   \       D - E  
-            \     /
-new    :     B - C
-
-
-"""
-
-
-fix1 = 1
-fix2 = 1
